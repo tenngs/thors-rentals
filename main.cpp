a8175823de3ebdd5af1e34a6085b33e33bc50df0
Eric Creates <tenngs@users.noreply.github.com>
2022-09-16 17:21:35 +0100

    call more table functions

--- main.cpp
+++ main.cpp
@@ -9,20 +9,6 @@ int main()
 
     // init app, create and populate tables with test data
     Application app{"Thor's Tantalising Arctic Equipment Rental AKA Thor's Rentals"};
-    Sql customersSqlStmnts;
-
-    // drop customers table if exists
-    Table customersDrop{"customers", "DROP"};
-    customersDrop.tableOperation(customersDrop.getOperationType(), customersDrop.getTableName(), customersSqlStmnts.getDropStmnt(customersDrop.getTableName()), true);
-
-    // create thors_rentals DB - this DB is used for all table operations
-    // create customers table
-    Table customersInit{"customers", "CREATE"};
-    customersInit.tableOperation(customersInit.getOperationType(), customersInit.getTableName(), customersSqlStmnts.getCreateStmnt(customersInit.getTableName()), true);
-
-    // insert test data into customers table
-    Table customersInsert{"customers", "INSERT INTO"};
-    customersInsert.tableOperation(customersInsert.getOperationType(), customersInsert.getTableName(), customersSqlStmnts.getInsertStmnt(customersInsert.getTableName()), true);
 
     // std::cout << customers.getSqlStatement() << std::endl;
     // if (Login::loginPrompt())
