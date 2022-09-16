046b30a3d8a533acae41a586be504faf7dd1087f
Eric Creates <tenngs@users.noreply.github.com>
2022-09-16 13:56:24 +0100

    call drop, create, insert statements for customers table

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
-
+#include <string>
+#include <iostream>
+#include "Application.h"
+#include "Sql.h"
+#include "Table.h"
 
 int main()
 {
 
-    // init app and tables
+    // init app, create and populate tables with test data
+    Application app{"Thor's Tantalising Arctic Equipment Rental AKA Thor's Rentals"};
+    Sql customersSqlStmnts;
 
+    // drop customers table if exists
+    Table customersDrop{"customers", "DROP"};
+    customersDrop.tableOperation(customersDrop.getOperationType(), customersDrop.getTableName(), customersSqlStmnts.getDropStmnt(customersDrop.getTableName()), true);
+
+    // create thors_rentals DB - this DB is used for all table operations
+    // create customers table
+    Table customersInit{"customers", "CREATE"};
+    customersInit.tableOperation(customersInit.getOperationType(), customersInit.getTableName(), customersSqlStmnts.getCreateStmnt(customersInit.getTableName()), true);
+
+    // insert test data into customers table
+    Table customersInsert{"customers", "INSERT INTO"};
+    customersInsert.tableOperation(customersInsert.getOperationType(), customersInsert.getTableName(), customersSqlStmnts.getInsertStmnt(customersInsert.getTableName()), true);
+
+    // std::cout << customers.getSqlStatement() << std::endl;
     // if (Login::loginPrompt())
     // {
     //     // TO DO: call table operation function to change
