1f75cdb8063fa681560a1567040ddf1a2ce3d942
Eric Creates <tenngs@users.noreply.github.com>
2022-09-17 18:52:04 +0100

    change variable names to more suitable ones

--- Table.h
+++ Table.h
@@ -10,7 +10,7 @@ private:
 
 public:
     Table(std::string nameVal, std::string opTypeVal);
-    int tableOperation(std::string operationType, std::string tableName, std::string sql, bool verbose = false);
+    int execTableOperation(std::string operationType, std::string tableName, std::string sql, bool verbose = false);
     std::string getSqlStatement();
     std::string getTableName();
     std::string getOperationType();
